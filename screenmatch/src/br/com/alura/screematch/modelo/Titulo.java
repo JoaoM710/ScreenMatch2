f09517b73e5420870c6280d07f255837b036958a
JoaoM710 <174065055+JoaoM710@users.noreply.github.com>
2025-04-14 19:49:41 -0300

    Add files via upload

--- screenmatch/src/br/com/alura/screematch/modelo/Titulo.java
+++ screenmatch/src/br/com/alura/screematch/modelo/Titulo.java
@@ -1,11 +1,9 @@
 package br.com.alura.screematch.modelo;
 
-import com.google.gson.annotations.SerializedName;
+import br.com.alura.screematch.excecao.ErroDeConversaoDeAnoException;
 
 public class Titulo implements Comparable<Titulo> {
-    @SerializedName("Title")
     private String nome;
-    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -19,6 +17,11 @@ public class Titulo implements Comparable<Titulo> {
 
     public Titulo(TituloOmdb meuTituloOmdb) {
         this.nome = meuTituloOmdb.title();
+
+        if (meuTituloOmdb.year().length() > 4){
+            throw new ErroDeConversaoDeAnoException("Não consegui converter o ano " +
+                    "porque tem mais de 04 caracteres!");
+        }
         this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
         this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0,2));
     }
@@ -84,6 +87,6 @@ public class Titulo implements Comparable<Titulo> {
 
     @Override
     public String toString() {
-        return "nome= " + nome + ", anoDeLancamento= " + anoDeLancamento + "," + "duração= " + duracaoEmMinutos;
+        return "(nome = " + nome + ", anoDeLancamento = " + anoDeLancamento + "," + "duração = " + duracaoEmMinutos + ")";
     }
 }
